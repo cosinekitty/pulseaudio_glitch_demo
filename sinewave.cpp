c60b3abdc401bd968de3e160faa5b99179d83c72
Don Cross <cosinekitty@gmail.com>
2022-12-19 19:12:38 -0500

    Successfully querying PulseAudio devices.

--- sinewave.cpp
+++ sinewave.cpp
@@ -9,11 +9,26 @@
 #include <vector>
 #include "RtAudio.h"
 
+void errorCallback(RtAudioErrorType type, const std::string &errorText)
+{
+    printf("errorCallback(%d): %s\n", type, errorText.c_str());
+}
+
 int main(int argc, const char *argv[])
 {
-    std::vector<RtAudio::Api> apis;
-    RtAudio::getCompiledApi(apis);
-    for (RtAudio::Api api : apis)
-        printf("Found API: %d\n", api);
+    RtAudio dac(RtAudio::LINUX_PULSE, &errorCallback);
+    if (dac.getCurrentApi() != RtAudio::LINUX_PULSE)
+    {
+        printf("Could not initialize PulseAudio.\n");
+        return 1;
+    }
+
+    std::vector<unsigned> deviceIdList = dac.getDeviceIds();
+    for (unsigned id : deviceIdList)
+    {
+        RtAudio::DeviceInfo info = dac.getDeviceInfo(id);
+        printf("    %u = [%s]\n", id, info.name.c_str());
+    }
+
     return 0;
 }
